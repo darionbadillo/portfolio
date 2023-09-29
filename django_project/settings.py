b7426d0655650e1bc47709ef58e5636056b7858b
darionbadillo <dbadillo@uccs.edu>
2023-09-29 16:15:34 -0600

    "Why isn't this working"

--- django_project/settings.py
+++ django_project/settings.py
@@ -10,10 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-<<<<<<< HEAD
-=======
 import os
->>>>>>> iter01
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -41,15 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-<<<<<<< HEAD
-=======
     #Add your app name here
     'portfolio_app',
 ]
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend'
->>>>>>> iter01
 ]
 
 MIDDLEWARE = [
@@ -130,8 +124,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-<<<<<<< HEAD
-=======
 STATICFILES_DIRS = [
 os.path.join(BASE_DIR, 'static')
 ]
@@ -139,7 +131,6 @@ os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/images/'
 
 
->>>>>>> iter01
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
