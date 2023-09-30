17e036d0d50f80fa31a62927e0b9ce6111f9a64f
darionbadillo <dbadillo@uccs.edu>
2023-09-29 20:06:53 -0600

    More modeling

--- portfolio_app/models.py
+++ portfolio_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Student(models.Model):
@@ -15,5 +16,14 @@ class Student(models.Model):
         )
     name = models.CharField(max_length=200)
     email = models.CharField("UCCS Email", max_length=200)
-    major = models.CharField(max_length=200, choices=MAJOR, blank = True)
+    major = models.CharField(max_length=200, choices=MAJOR, blank = False)
 
+    #Define default String to return the name for representing the Model object."
+    def __str__(self):
+        return self.name
+
+    #Returns the URL to access a particular instance of MyModelName.
+    #if you define this method then Django will automatically
+    # add a "View on Site" button to the model's record editing screens in the Admin site
+    def get_absolute_url(self):
+        return reverse('student-detail', args=[str(self.id)])
