0c9aa80380ae20e3a2a4a65bd5b7a1e6e2153215
darionbadillo <dbadillo@uccs.edu>
2023-10-17 19:24:11 -0600

    yes

--- django_project/urls.py
+++ django_project/urls.py
@@ -16,10 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from portfolio_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     
     #connect path to portfolio_app urls
     path('', include('portfolio_app.urls')),
+    path('students/', views.StudentListView.as_view(), name= 'students'),
+    path('student/<int:pk>', views.StudentDetailView.as_view(), name='student-detail'),
+
 ]
--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.views import generic
+from .models import Student
+class StudentListView(generic.ListView):
+    model = Student
+class StudentDetailView(generic.DetailView):
+    model = Student
+    
 # Create your views here.
 def index(request):
-    
     # Render the HTML template index.html with the data in context variable
     return render(request, 'portfolio_app/index.html')
+    
